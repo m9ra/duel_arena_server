02d8bce8a7e26673a3c5a6ddf2ed0cb2363b3e2c
mvodolan@cz.ibm.com <mvodolan@cz.ibm.com>
2018-10-13 14:22:23 +0200

    Add missing files.

---
+++ networking/websocket.py
@@ -0,0 +1,61 @@
+import json
+from queue import Queue
+from threading import Thread, RLock
+
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+
+_L_websockets = RLock()
+_clients = []
+_data_to_send = Queue()
+
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        with _L_websockets:
+            _clients.append(self)
+
+    def on_message(self, message):
+        pass  # no incomming messages are expected
+
+    def on_close(self):
+        with _L_websockets:
+            _clients.remove(self)
+
+    def check_origin(self, origin):
+        return True
+
+
+def broadcast(data):
+    _data_to_send.put(data)
+
+
+def run_listener(websocket_port):
+    def _run():
+        application = tornado.web.Application([
+            (r'/ws', WSHandler),
+        ])
+
+        http_server = tornado.httpserver.HTTPServer(application)
+        http_server.listen(websocket_port)
+        tornado.ioloop.IOLoop.instance().start()
+
+    th = Thread(target=_run, daemon=True)
+    th.start()
+
+    sender = Thread(target=_sender, daemon=True)
+    sender.start()
+
+
+def _sender():
+    while True:
+        data = _data_to_send.get()
+        if not data:
+            continue
+
+        json_data = json.dumps(data)
+        with _L_websockets:
+            for client in _clients:
+                client.write_message(json_data)
---
+++ static/live_manager.js
@@ -0,0 +1,91 @@
+var LiveManager = function(boardIds, websocketPort){
+    var self = {};
+    var boards = [];
+    var idToBoard = {};
+
+    for(var id in boardIds){
+        var boardId = boardIds[id];
+        var board = Board(boardId);
+        board.finishTime = 0;
+        boards.push(board);
+    }
+
+
+    self.start = function(){
+        idToBoard = {};
+
+        var wsPath = "ws://" + document.location.hostname + ":" + websocketPort + "/ws";
+        var ws = new WebSocket(wsPath);
+
+        ws.onmessage = function(evt) {self._dataArrived($.parseJSON(evt.data))};
+        ws.onclose = function(evt) { setTimeout(self.start, 1000); };
+        ws.onopen = function(evt) { };
+    }
+
+    self._dataArrived = function(data){
+        if("board" in data){
+            var board = self._tryFindFreeBoard();
+            if(board == null)
+                return;
+
+            var boardData = data["board"];
+            board.clear();
+            board.players(boardData["player1"], boardData["player2"]);
+            idToBoard[boardData["_id"]] = board;
+
+            // in case board came with some moves already, play them
+            var moves = boardData["moves"];
+            for(var moveId in moves){
+                var move = moves[moveId];
+                board.move(move[0], move[1]);
+            }
+        }else if("finished_board" in data){
+            var id = data["finished_board"]["_id"];
+            if(id in idToBoard){
+                idToBoard[id].finishTime = new Date().getTime();
+                delete idToBoard[id];
+            }
+        }else if("move" in data){
+            var id = data["_id"];
+            var move = data["move"];
+            if(id in idToBoard){
+                idToBoard[id].move(move[0],move[1]);
+            }
+        }else{
+            console.warning(data);
+        }
+    }
+
+    self._tryFindFreeBoard = function(){
+        var freeBoards = [];
+
+        for(var boardId in boards){
+            var board = boards[boardId];
+            if(self._isBoardAvailable(board)){
+                freeBoards.push(board);
+            }
+        }
+
+        if(freeBoards.length == 0)
+            return null;
+
+        freeBoards.sort(function(a, b){return b.finishTime - a.finishTime})
+
+        return freeBoards[0];
+    }
+
+    self._isBoardAvailable = function(board) {
+        if(new Date().getTime() - board.finishTime < 5000)
+            // keep the finished game displayed for some time
+            return false;
+
+        for(var id in idToBoard){
+            if(board == idToBoard[id])
+                return false;
+        }
+
+        return true;
+    }
+
+    return self;
+}
